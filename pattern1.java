e10b64215bb34893e63e0003d921c908d175eac7
Srishti Raut <srishti.raut1102000@gmail.com>
2026-07-19 13:14:18 +0530

    pgm: pattern printing

---
+++ pattern1.java
@@ -0,0 +1,21 @@
+//Java pgm to print this patter
+// *
+// **
+// ***
+// ****
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	      //outer loop controls rows
+	      //inner loop controls pattern printing
+	      
+	      for(int i=1; i<=4;i++){
+	          for(int j=1; j<=i;j++){
+	              System.out.print("* ");
+	          }
+	          System.out.println(" ");
+	      }
+	}
+}
