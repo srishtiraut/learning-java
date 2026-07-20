605c5c7e3b2578b43573daddec81dc949f9f782e
Srishti Raut <srishti.raut1102000@gmail.com>
2026-07-20 20:09:43 +0530

    pgm: pattern printing 4

---
+++ pattern4.java
@@ -0,0 +1,20 @@
+//Java pgm to print this pattern
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	      //outer loop controls rows
+	      //inner loop controls pattern printing
+	      
+	     for(int i=1; i<=4; i++){
+	         for(int j=1; j<=i;j++){
+	             System.out.print(i+" ");
+	         }
+	         System.out.println(" ");
+	     }
+	}
+}
